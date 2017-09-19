4c3a71bed7e9cf97b1028696385666ce3af3b9d7
nikita <nikitatroshenko@gmail.com>
2017-09-19 15:56:58 +0300

    Added kasiski circuit stub

---
+++ kasiski.c
@@ -0,0 +1,4 @@
+//
+// Created by nikita on 19.9.17.
+//
+#include "kasiski.h"
---
+++ kasiski.h
@@ -0,0 +1,12 @@
+//
+// Created by nikita on 19.9.17.
+//
+
+#ifndef DSS_VIGENERE_KASISKI_H
+#define DSS_VIGENERE_KASISKI_H
+
+struct kasiski {
+
+};
+
+#endif //DSS_VIGENERE_KASISKI_H
